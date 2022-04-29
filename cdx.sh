2474d9b1fcc8fff3b49a505655880cde6028d890
Jessy Varin <jessy.varin@epitech.eu>
2022-04-29 11:49:29 +0200

    first argument used as filename, second as path and defaults to home directory

--- cdx.sh
+++ cdx.sh
@@ -1,21 +1,29 @@
-#! bin/bash
+#! /bin/bash
+
+set -x
 
 path=""
 if [ -n "$1" ]
 then
-	case $1 in
-		"-h")
-			path="~/" ;;
-		"-c")
-			IFS=' ' read -ra keywords <<< "$2"
-			for i in "${keywords[@]}"
-			do
-				path+="/**/*$i*"
-			done ;;
-	esac
+	if [ -n "$2" ]
+	then
+		path="$2"
+	else
+		path=~/
+	fi
+
+#	case $1 in
+#		"-h")
+#			path="~/" ;;
+#		"-c")
+#			IFS=' ' read -ra keywords <<< "$2"
+#			for i in "${keywords[@]}"
+#			do
+#				path+="/**/*$i*"
+#			done ;;
+#	esac
+
+	cd $(find $path -type d -name "$1" 2>&1 | grep -v "Permission denied" | head -n 1)
 fi
-echo $path
 
-cd $(find $($path | sed 's/"//g') -type d | head -n 1)
-#cd $(find / -type d -name "$path" 2>&1 | grep -v "Permission denied" | head -n 1)
-
+set +x
